95541c9b85a8f4e442b5ab77a59d4a05a39d5236
chenzh <czh533143@163.com>
2020-03-20 22:35:06 +0800

    地图绘制成功

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # draw3dEcharts
 
 ## 项目描述
+利用定时器自动绘制3d图形显示以及状态更新
+
+## 技术
 ```
-  利用定时器自动绘制3d图形显示以及状态更新
+echarts、echarts-gl、prism、 echarts地图数据：guangdong.js
 ```
+
 ## 伤心之处
 第二次写了，第一次被莫名的删除了
--- main.js
+++ main.js
@@ -1,21 +1,139 @@
 ~ function () {
-    var duration = 50
+    let duration = 50
     let myChart = null,
         option = null
-    let code = `// 基于准备好的dom，初始化echarts实例
+    //地图数据    
+    const mapData = [{
+            "name": "阳江市",
+            "value": [
+                111.989062,
+                21.858167,
+                100
+            ],
+            itemStyle: {
+                color: '#17e3ff',
+                opacity: 1
+            },
+        },
+        {
+            "name": "惠州市",
+            "value": [
+                114.42356,
+                23.113434,
+                100,
+            ],
+            itemStyle: {
+                color: '#fffe00',
+                opacity: 1
+            },
+        },
+        {
+            "name": "清远市",
+            "value": [
+                113.057866,
+                23.690082,
+                100,
+            ],
+            itemStyle: {
+                color: '#18d0ff',
+                opacity: 1
+            },
+        },
+        {
+            "name": "梅州市",
+            "value": [
+                116.126091,
+                24.291806,
+                100,
+            ],
+            itemStyle: {
+                color: '#e94971',
+                opacity: 1
+            },
+        },
+        {
+            "name": "湛江市",
+            "value": [
+                110.358655,
+                21.280766,
+                100,
+            ],
+            itemStyle: {
+                color: '#634fd4',
+                opacity: 1
+            },
+        },
+    ];
+    //模板文字
+    let code = `/* 基于准备好的dom，初始化echarts实例 */
         myChart = echarts.init(document.getElementById('main'));
-        option={
-
+        option = {
+            geo3D: {
+                map: '广东',
+                groundPlane:{},
+                light:{},
+                viewControl:{}
+            },
+            series: {
+                type: 'bar3D',
+                coordinateSystem: 'geo3D',
+                data:[]
             }
-        // 显示图表
+        }
+        /* 显示图表 */
         myChart.setOption(option);
+        /* 设置视角中心点 */
+        option.geo3D.viewControl.center=[-10, 0, 10]
+        /* 设置最大选择角度 */
+        option.geo3D.viewControl.minBeta=-360
+        option.geo3D.viewControl.maxBeta=360
+        /* 开启旋转 */
+        option.geo3D.viewControl.autoRotate=true
+        /* 设置背景颜色 */
+        option.geo3D.environment='black'
+        /* 设置3d地图颜色 */
+        option.geo3D.itemStyle={
+            borderColor: 'rgb(62,215,213)',
+            areaColor: '#4490fc',
+            borderWidth: 1
+        }
+        /* 设置光照 */
+        option.geo3D.light.main={
+                intensity: 1,
+                shadow: true,
+                alpha: 120,
+                beta: 100
+        }
+        /* 添加3d柱图 */
+        option.series.data=mapData
+        /* 添加3d柱图标签 */
+        option.series.label={
+            show: true,
+            formatter: '{b}',
+            position: 'top',
+            textStyle: {
+                color: '#000',
+                backgroundColor: '#fff',
+            }
+        }
+        /* 开始3d柱图光照渲染 */
+        option.series.shading='lambert'
         `
 
+
+    //绘制
     function writeCode(prefix, code, fn) {
         let domCode = document.querySelector('.code')
-        let styleTag = document.querySelector('#styleTag')
-        let n = 0,sum = 0
+        let n = 0,
+            m = 0,
+            sum = 0,
+            str = ''
         let id = null
+        const arr = ["myChart.setOption(option);", "/* 设置视角中心点 */", "/* 设置最大选择角度 */",
+            "/* 开启旋转 */", "/* 设置背景颜色 */","/* 设置3d地图颜色 */",
+            "/* 设置光照 */","/* 添加3d柱图 */","/* 添加3d柱图标签 */",
+            "/* 开始3d柱图光照渲染 */"
+        ]
         id = setTimeout(function run() {
             n += 1
             domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css)
@@ -24,8 +142,17 @@
                 id = setTimeout(run, duration)
             } else {
                 fn && fn.call()
+                eval(code.substring(m) + '\n' + arr[0])
             }
+            if (code.substring(0, n).indexOf(arr[sum]) !== -1) {
+                str = code.substring(m, n)
+                m = n;
+                sum > 0 ? eval(str + '\n' + arr[0]) : eval(str)
+                sum++;
+            }
+            
         }, duration)
+        // eval(code)
     }
 
     writeCode('', code)
